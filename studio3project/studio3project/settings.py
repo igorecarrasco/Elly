bb0575952cf266669719fc369ca909f84d95c014
John Schimmel <john.schimmel@dowjones.com>
2016-10-26 15:32:27 -0400

    saltfake for docker servo build

--- studio3project/studio3project/settings.py
+++ studio3project/studio3project/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('saltkey')
+SECRET_KEY = os.getenv('saltkey','dockerbuilding')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
