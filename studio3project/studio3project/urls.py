c2ad26f4066a8a62e0222d170694beb713a29103
John Schimmel <john.schimmel@dowjones.com>
2016-10-26 14:21:37 -0400

    docker update with health check

--- studio3project/studio3project/urls.py
+++ studio3project/studio3project/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^twitlister/', include('twitlister.urls')),
     url(r'^elly/', include('elly.urls')),
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^_health', include('elly.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
