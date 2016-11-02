e0cab94a78967516bff152629c597c20aa44c4b6
igorecarrasco <igor.ecarrasco@gmail.com>
2016-11-02 13:35:42 -0400

    Changesss

--- studio3project/elly/views.py
+++ studio3project/elly/views.py
@@ -5,9 +5,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Elly
+import urllib2
+import json
+import os
+from os.path import join,dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__),'..','..','.env')
+load_dotenv(dotenv_path)
+token = os.getenv('parselytoken')
+apikey= os.getenv('parselyapikey')
 
 def index(request):
 	elly_list = Elly.objects.order_by('-id')
 	template = loader.get_template('elly/index.html')
 	context = {'elly_list': elly_list,}
 	return HttpResponse(template.render(context,request))
+
+def hits(request):  
+		url = "http://api.parsely.com/v2/analytics/post/detail?apikey="+apikey+"&secret="+token+"&url="+context
+		resposta = urllib2.urlopen(url)
+		hits = json.load(resposta)['data'][0]['visitors']
+		return HttpResponse(hits)
+		print hits
+
