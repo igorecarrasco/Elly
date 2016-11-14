a4128f4a42d44192e3d77d92332bca3d17ee6c94
igorecarrasco <igor.ecarrasco@gmail.com>
2016-11-14 12:13:43 -0500

    oauth experimentation

--- studio3project/elly/views.py
+++ studio3project/elly/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Elly
 import re
@@ -14,6 +14,7 @@ from os.path import join,dirname
 from dotenv import load_dotenv
 from django.utils.encoding import smart_str, smart_unicode
 from requests_oauthlib import OAuth1Session, OAuth1
+from django.core.urlresolvers import reverse
 import requests
 
 dotenv_path = join(dirname(__file__),'..','..','.env')
@@ -24,44 +25,46 @@ ckey = os.getenv('ckeysf')
 csecret = os.getenv('csecretsf')
 suid = os.getenv('serviceuserid')
 
-request_token_url = 'https://www.socialflow.com/oauth/request_token?oauth_callback=oob'
-oauth = OAuth1Session(ckey, client_secret=csecret)
 
-fetch_response = oauth.fetch_request_token(request_token_url)
-resource_owner_key = fetch_response.get('oauth_token')
-resource_owner_secret = fetch_response.get('oauth_token_secret')
+# print 'Please go her and authorize', authorize_url
+# verifier = raw_input('Plaese input the verifier')
 
-base_authorization_url = 'https://www.socialflow.com/oauth/authorize'
-
-authorize_url = base_authorization_url + '?oauth_token='
-authorize_url = authorize_url + resource_owner_key
-print 'Please go her and authorize', authorize_url
-verifier = raw_input('Plaese input the verifier')
-
-access_token_url = 'https://www.socialflow.com/oauth/access_token'
-
-oauth = OAuth1Session(ckey,
-	client_secret=csecret,
-	resource_owner_key=resource_owner_key,
-	resource_owner_secret=resource_owner_secret,
-	verifier=verifier)
-
-oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-resource_owner_key = oauth_tokens.get('oauth_token')
-resource_owner_secret = oauth_tokens.get('oauth_token_secret')
-
-headeroauth = OAuth1(ckey,
-	csecret,
-	resource_owner_key,
-	resource_owner_secret,
-	signature_type='auth_header')
-
-def index(request):
+def lister(request):
+	#process taking on from where we left off on index
+	verifier = request.GET.get('oauth_verifier')
+	access_token_url = 'https://www.socialflow.com/oauth/access_token'
+	# --- HERE IS WHERE I STOPPED, NEED TO FIGURE OUT SESSIONS TO GET RESOURCE OWNER KEY, SECRET WORKING. --- 
+	oauth = OAuth1Session(ckey,
+		client_secret=csecret,
+		resource_owner_key=resource_owner_key,
+		resource_owner_secret=resource_owner_secret,
+		verifier=verifier)
+	oauth_tokens = oauth.fetch_access_token(access_token_url)
+	resource_owner_key = oauth_tokens.get('oauth_token')
+	resource_owner_secret = oauth_tokens.get('oauth_token_secret')
+	headeroauth = OAuth1(ckey,
+		csecret,
+		resource_owner_key,
+		resource_owner_secret,
+		signature_type='auth_header')
 	elly_list = Elly.objects.order_by('-id')
 	template = loader.get_template('elly/index.html')
 	context = {'elly_list': elly_list,}
 	return HttpResponse(template.render(context,request))
+
+def index(request):
+	#first steps of oauth1 login
+	urlcallback = request.build_absolute_uri(reverse('lister'))
+	request_token_url = 'https://www.socialflow.com/oauth/request_token'+ '?oauth_callback=' + urlcallback
+	oauth = OAuth1Session(ckey, client_secret=csecret)
+	fetch_response = oauth.fetch_request_token(request_token_url)
+	resource_owner_key = fetch_response.get('oauth_token')
+	resource_owner_secret = fetch_response.get('oauth_token_secret')
+	base_authorization_url = 'https://www.socialflow.com/oauth/authorize'
+	authorize_url = base_authorization_url + '?oauth_token=' 
+	authorize_url = authorize_url + resource_owner_key + '&oauth_callback=' + urlcallback
+	#drive user to lister view, where rest of authorization can take place
+	return HttpResponseRedirect(authorize_url)
 
 def socialflow(request):
 	urlsocialflow = "https://api.socialflow.com/feed/list?account_type=twitter&limit=3&service_user_id="+suid
