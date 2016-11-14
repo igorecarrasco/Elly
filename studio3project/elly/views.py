a51b9f3ebadb3ef73ecb8fb07c1ab8c309e2956d
igorecarrasco <igor.ecarrasco@gmail.com>
2016-11-13 20:39:48 -0500

    Tweet Line view

--- studio3project/elly/views.py
+++ studio3project/elly/views.py
@@ -16,46 +16,46 @@ from django.utils.encoding import smart_str, smart_unicode
 from requests_oauthlib import OAuth1Session, OAuth1
 import requests
 
-
 dotenv_path = join(dirname(__file__),'..','..','.env')
 load_dotenv(dotenv_path)
 token = os.getenv('parselytoken')
 apikey= os.getenv('parselyapikey')
 ckey = os.getenv('ckeysf')
 csecret = os.getenv('csecretsf')
+suid = os.getenv('serviceuserid')
 
-# request_token_url = 'https://www.socialflow.com/oauth/request_token?oauth_callback=oob'
-# oauth = OAuth1Session(ckey, client_secret=csecret)
+request_token_url = 'https://www.socialflow.com/oauth/request_token?oauth_callback=oob'
+oauth = OAuth1Session(ckey, client_secret=csecret)
 
-# fetch_response = oauth.fetch_request_token(request_token_url)
-# resource_owner_key = fetch_response.get('oauth_token')
-# resource_owner_secret = fetch_response.get('oauth_token_secret')
+fetch_response = oauth.fetch_request_token(request_token_url)
+resource_owner_key = fetch_response.get('oauth_token')
+resource_owner_secret = fetch_response.get('oauth_token_secret')
 
-# base_authorization_url = 'https://www.socialflow.com/oauth/authorize'
+base_authorization_url = 'https://www.socialflow.com/oauth/authorize'
 
-# authorize_url = base_authorization_url + '?oauth_token='
-# authorize_url = authorize_url + resource_owner_key
-# print 'Please go her and authorize', authorize_url
-# verifier = raw_input('Plaese input the verifier')
+authorize_url = base_authorization_url + '?oauth_token='
+authorize_url = authorize_url + resource_owner_key
+print 'Please go her and authorize', authorize_url
+verifier = raw_input('Plaese input the verifier')
 
-# access_token_url = 'https://www.socialflow.com/oauth/access_token'
+access_token_url = 'https://www.socialflow.com/oauth/access_token'
 
-# oauth = OAuth1Session(ckey,
-# 	client_secret=csecret,
-# 	resource_owner_key=resource_owner_key,
-# 	resource_owner_secret=resource_owner_secret,
-# 	verifier=verifier)
+oauth = OAuth1Session(ckey,
+	client_secret=csecret,
+	resource_owner_key=resource_owner_key,
+	resource_owner_secret=resource_owner_secret,
+	verifier=verifier)
 
-# oauth_tokens = oauth.fetch_access_token(access_token_url)
+oauth_tokens = oauth.fetch_access_token(access_token_url)
 
-# resource_owner_key = oauth_tokens.get('oauth_token')
-# resource_owner_secret = oauth_tokens.get('oauth_token_secret')
+resource_owner_key = oauth_tokens.get('oauth_token')
+resource_owner_secret = oauth_tokens.get('oauth_token_secret')
 
-# headeroauth = OAuth1(ckey,
-# 	csecret,
-# 	resource_owner_key,
-# 	resource_owner_secret,
-# 	signature_type='auth_header')
+headeroauth = OAuth1(ckey,
+	csecret,
+	resource_owner_key,
+	resource_owner_secret,
+	signature_type='auth_header')
 
 def index(request):
 	elly_list = Elly.objects.order_by('-id')
@@ -63,21 +63,28 @@ def index(request):
 	context = {'elly_list': elly_list,}
 	return HttpResponse(template.render(context,request))
 
-# def socialflow(request):
-# 	urlsocialflow = "https://api.socialflow.com/account/list?account_type=twitter&limit=3"
-# 	r = oauth.get(urlsocialflow)
-# 	return HttpResponse(r)
+def socialflow(request):
+	urlsocialflow = "https://api.socialflow.com/feed/list?account_type=twitter&limit=3&service_user_id="+suid
+	r = oauth.get(urlsocialflow)
+	return HttpResponse(r)
 
 def rssfeed(request):
 	if request.method == "POST":
-		f=open('elly/rssfeed.rss','w')
-		f.write('<?xml version="1.0" encoding="UTF-8"?><channel><title>Weekend Social Scheduling - Tweets</title><link>http://online.wsj.com/page/2_0062.html</link><atom:link type="application/rss+xml" rel="self" href="http://online.wsj.com/page/2_0062.html"/><description>Social Headlines for Methode Articles</description><language>en-us</language><pubDate>Wed, 02 Nov 2016 18:19:52 -0400</pubDate><lastBuildDate>Wed, 02 Nov 2016 18:19:52 -0400</lastBuildDate><copyright>Dow Jones &amp; Company, Inc.</copyright><generator>http://online.wsj.com/page/2_0062.html</generator><docs>http://cyber.law.harvard.edu/rss/rss.html</docs><image><title>Social Headlines for Methode Articles</title><link>http://online.wsj.com/page/2_0062.html</link><url>http://online.wsj.com/img/wsj_sm_logo.gif</url></image>')
-	postids = request.POST.getlist('postid','')
-	for element in postids:
-		objetoelly = Elly.objects.get(id=element)
-		titulo = objetoelly.title
-		f.write('<item><title>'+titulo+'</title><link>'+objetoelly.link+'</link><description><![CDATA[]]></description><content:encoded/><pubDate><![CDATA[]]></pubDate><guid isPermaLink="false"><![CDATA[]]></guid><category domain="AccessClassName">FREE</category></item>')
-		f.write('</channel></rss>')
+		postids = request.POST.getlist('postid','')
+		for element in postids:
+			objetoelly = Elly.objects.get(id=element)
+			titulo = objetoelly.title
+			link = objetoelly.link
+			urltwit = "https://api.socialflow.com/message/add?service_user_id="+suid+"&account_type=twitter&message="+titulo+" "+link+"&publish_option=hold&shorten_links=1"
+			r = oauth.get(urltwit)
+	# 	f=open('elly/rssfeed.rss','w')
+	# 	f.write('<?xml version="1.0" encoding="UTF-8"?><channel><title>Weekend Social Scheduling - Tweets</title><link>http://online.wsj.com/page/2_0062.html</link><atom:link type="application/rss+xml" rel="self" href="http://online.wsj.com/page/2_0062.html"/><description>Social Headlines for Methode Articles</description><language>en-us</language><pubDate>Wed, 02 Nov 2016 18:19:52 -0400</pubDate><lastBuildDate>Wed, 02 Nov 2016 18:19:52 -0400</lastBuildDate><copyright>Dow Jones &amp; Company, Inc.</copyright><generator>http://online.wsj.com/page/2_0062.html</generator><docs>http://cyber.law.harvard.edu/rss/rss.html</docs><image><title>Social Headlines for Methode Articles</title><link>http://online.wsj.com/page/2_0062.html</link><url>http://online.wsj.com/img/wsj_sm_logo.gif</url></image>')
+	# postids = request.POST.getlist('postid','')
+	# for element in postids:
+	# 	objetoelly = Elly.objects.get(id=element)
+	# 	titulo = objetoelly.title
+	# 	f.write('<item><title>'+titulo+'</title><link>'+objetoelly.link+'</link><description><![CDATA[]]></description><content:encoded/><pubDate><![CDATA[]]></pubDate><guid isPermaLink="false"><![CDATA[]]></guid><category domain="AccessClassName">FREE</category></item>')
+	# 	f.write('</channel></rss>')
 	template = loader.get_template('elly/rssfeed.html')
 	context = {'postids':postids,}
   	return HttpResponse(template.render(context,request))
