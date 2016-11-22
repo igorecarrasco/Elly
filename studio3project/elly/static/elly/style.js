ac4fc10f256389b77fb40f854a2ac1663547d114
igorecarrasco <igor.ecarrasco@gmail.com>
2016-11-21 20:09:20 -0500

    Fixed all bugs (hopefully) with schedule/optimize showing or not

--- studio3project/elly/static/elly/style.js
+++ studio3project/elly/static/elly/style.js
@@ -43,36 +43,66 @@ $('#socialselector').change(function(){
                                   };
                                   $( ".optimizefield" ).hide();
                                   $( ".reveal-if-active").hide();
+
                       } else if (socialstate[0] == 'twitter') {
                                   var styles = {
                                   background : '#95D7F9',
                                   };
                                   $( ".optimizefield" ).show();
+                                 if ($(".optimizefield").is(":not(:checked)")){
+                                    $('.reveal-if-active').hide();
+                                    } 
+                                    if ($(".optimizefield").is(":checked")) {
+                                    $('.reveal-if-active').show();
+                                  }
                       }
                         else if (socialstate[0] == 'linked_in_page') {
                                   var styles = {
                                   background : '#0077b5',
                                   };
                                   $( ".optimizefield" ).show();
+                                    if ($(".optimizefield").is(":not(:checked)")){
+                                    $('.reveal-if-active').hide();
+                                    } 
+                                    if ($(".optimizefield").is(":checked")) {
+                                    $('.reveal-if-active').show();
+                                  }
                       }
-                      else if (socialstate[0] == 'google_plus_page') {
+                        else if (socialstate[0] == 'google_plus_page') {
                                   var styles = {
                                   background : '#d34836',
                                   };
                                   $( ".optimizefield" ).show();  
-                      }
+                                  if ($(".optimizefield").is(":not(:checked)")){
+                                    $('.reveal-if-active').hide();
+                                    } 
+                                    if ($(".optimizefield").is(":checked")) {
+                                    $('.reveal-if-active').show();
+                      }}
                       elemento.parent('.ellybox').css( styles );
-                    });
+});
 $(document).ready(function(){
   $('#startoptimization').AnyTime_picker( "field1",
     { format: "%W, %M %D in the Year %z %E", firstDOW: 1 } );
     $('#scheduletime').AnyTime_picker( "field1",
     { format: "%W, %M %D in the Year %z %E", firstDOW: 1 } );
 });
+$(document).ready(function(){
+  $('.reveal-if-active').hide();
+  $('.reveal-if-active2').hide();
+});
+$("input:radio").change(function(){
+  if ($(".optimizefield").is(":not(:checked)")){
+      $('.reveal-if-active').hide();
+      } 
+      if ($(".optimizefield").is(":checked")) {
+      $('.reveal-if-active').show();
+    }
+      if ($(".schedulefield").is(":not(:checked)")){
+      $('.reveal-if-active2').hide();
+      } 
+      if ($(".schedulefield").is(":checked")) {
+      $('.reveal-if-active2').show();
+    }
+  });
 
-// if ($(".optimizefield").is(":not(:checked)")){
-//   $('reveal-if-active').hide();
-// }
-// else {
-//   $('reveal-if-active').show();
-// }
